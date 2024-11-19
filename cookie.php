fe65f0574d1deaa437f05285dfe1fc5082a1565a
raksha0774 <raksha0774@gmail.com>
2024-11-20 01:21:08 +0530

    Create cookie.php

---
+++ cookie.php
@@ -0,0 +1,21 @@
+<?php
+session_start(); // Always start the session at the top of the script
+
+// Check if the 'views' session variable exists
+if (!isset($_SESSION['views'])) { 
+    $_SESSION['views'] = 1; // Initialize 'views' if it doesn't exist
+} else {
+    $_SESSION['views']++; // Increment 'views' if it exists
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Views Counter</title>
+</head>
+<body>
+    <h2>Page Views Counter</h2>
+    <p>This page has been viewed <?php echo $_SESSION['views']; ?> times.</p>
+    <p>Refresh the page to see the count increase.</p>
+</body>
+</html>
